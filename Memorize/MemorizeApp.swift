1874a278f3632560bf1bd4f98e440c395468a2d5
Hannes Richter <scarafleus@gmail.com>
2024-11-19 14:28:47 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hannes Richter on 19.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Hannes Richter on 19.11.24.
+//
+
+import Testing
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
