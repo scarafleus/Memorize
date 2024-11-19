e13381ea5874371cf8de52b4a610d36263ca4904
Hannes Richter <scarafleus@gmail.com>
2024-11-19 16:15:22 +0100

    CardView AspectRatio Cards ScrollView Segmented ContentView Finished Lesson 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🥸", "😰", "😉", "😨", "😨"]
+    let emojis = ["🥸", "😰", "😉", "😨", "☺️", "🤢", "🥹", "😌", "🙃", "😅", "😭"]
+    @State var cardCount = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
+        .imageScale(.large)
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardCountAdjuster(by: -1, symbol: "minus.square.fill")
+            Spacer()
+            cardCountAdjuster(by: 1, symbol: "plus.square.fill.on.square.fill")
+        }
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset >= emojis.count)
     }
 }
 #Preview {
@@ -31,13 +60,13 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .aspectRatio(2/3, contentMode: .fit)
         .onTapGesture {
