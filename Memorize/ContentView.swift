a865b00d5d901bec843d424522f0095e119f5390
Hannes Richter <scarafleus@gmail.com>
2024-11-19 15:21:43 +0100

    Toggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -25,20 +25,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🥸").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
         }
         .aspectRatio(2/3, contentMode: .fit)
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
